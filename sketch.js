b04ec8fa6bfaf896c2b54e2ced9399a614907360
Andrea Elena Febres Medina <andreae.febresm@hotmail.it>
2018-11-05 23:11:23 +0100

    Webcam added

--- sketch.js
+++ sketch.js
@@ -1,11 +1,24 @@
-function preload(){
-  // put preload code here
-}
+var capture;
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  capture = createCapture(VIDEO);
+  capture.size(windowWidth, windowHeight);
+  capture.hide();
 }
 
 function draw() {
-  // put drawing code here
+
+  var myImage = capture.loadPixels();
+
+
+
+  image(myImage, 0, 0, windowWidth, windowHeight);
+  if (mouseX > windowWidth / 3 && mouseX < windowWidth * 2 / 3 && mouseY > windowHeight*2/ 5 && mouseY < windowHeight*3/5) {
+  rotate(180);
+  }
+  rectMode(CENTER);
+  noFill();
+  strokeWeight(5);
+  rect(windowWidth / 2, windowHeight / 2, windowWidth / 3, windowHeight / 5);
 }
